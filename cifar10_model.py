f41a4ee50f7ebdac19174542c656bc92b8844f28
zhuangzi926 <zhuangzi926@qq.com>
2020-02-28 21:21:38 +0800

    change tf.keras.layers.add() to tf.keras.layers.Add()()

--- cifar10_model.py
+++ cifar10_model.py
@@ -1,53 +1,60 @@
 import tensorflow as tf
 import tensorflow.keras as keras
-from keras.layers import Dense, Conv2D, BatchNormalization, Activation
-from keras.layers import AveragePooling2D, InputLayer, Flatten, MaxPool2D, Dropout
+from keras.layers import Dense, Conv2D, BatchNormalization, Activation, Add
+from keras.layers import AveragePooling2D, InputLayer, Flatten, MaxPool2D, Dropout, Input
 from keras.optimizers import Adam
 from keras.regularizers import l2
 from keras.models import Model, Sequential
+import numpy as np
 
 
-class Resnet_layer(keras.layers.Layer):
-    def __init__(
-        self,
-        num_filters=16,
-        kernel_size=3,
-        strides=1,
-        activation="relu",
-        batch_normalization=True,
-        conv_first=True,
-    ):
-        super().__init__()
-        self.num_filters = num_filters
-        self.kernel_size = kernel_size
-        self.strides = strides
-        self.activation = activation
-        self.batch_normalization = batch_normalization
-        self.conv_first = conv_first
-        self.conv = Conv2D(
-            num_filters,
-            kernel_size=kernel_size,
-            strides=strides,
-            padding="same",
-            kernel_initializer="he_normal",
-            kernel_regularizer=l2(1e-4),
-        )
+def resnet_layer(
+    inputs,
+    num_filters=16,
+    kernel_size=3,
+    strides=1,
+    activation="relu",
+    batch_normalization=True,
+    conv_first=True,
+):
+    """2D Convolution-Batch Normalization-Activation stack builder
 
-    def call(self, inputs):
-        x = inputs
-        if self.conv_first:
-            x = self.conv(x)
-            if self.batch_normalization:
-                x = BatchNormalization()(x)
-            if self.activation is not None:
-                x = Activation(self.activation)(x)
-        else:
-            if self.batch_normalization:
-                x = BatchNormalization()(x)
-            if self.activation is not None:
-                x = Activation(self.activation)(x)
-            x = self.conv(x)
-        return x
+    # Arguments
+        inputs (tensor): input tensor from input image or previous layer
+        num_filters (int): Conv2D number of filters
+        kernel_size (int): Conv2D square kernel dimensions
+        strides (int): Conv2D square stride dimensions
+        activation (string): activation name
+        batch_normalization (bool): whether to include batch normalization
+        conv_first (bool): conv-bn-activation (True) or
+            bn-activation-conv (False)
+
+    # Returns
+        x (tensor): tensor as input to the next layer
+    """
+    conv = Conv2D(
+        num_filters,
+        kernel_size=kernel_size,
+        strides=strides,
+        padding="same",
+        kernel_initializer="he_normal",
+        kernel_regularizer=l2(1e-4),
+    )
+
+    x = inputs
+    if conv_first:
+        x = conv(x)
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+    else:
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+        x = conv(x)
+    return x
 
 
 class Teacher_model(tf.keras.Model):
@@ -67,27 +74,29 @@ class Teacher_model(tf.keras.Model):
     def call(self, inputs):
         num_filters = 16
         num_res_blocks = int((self.depth - 2) / 6)
-        inputs = InputLayer(input_shape=(32, 32, 3))
-        x = Resnet_layer()(inputs=inputs)
+        inputs = tf.convert_to_tensor(inputs, dtype=tf.float32)
+        inputs = InputLayer(input_shape=(32, 32, 3))(inputs)
+        x = resnet_layer(inputs=inputs)
         # Instantiate the stack of residual units
         for stack in range(3):
             for res_block in range(num_res_blocks):
                 strides = 1
                 if stack > 0 and res_block == 0:  # first layer but not first stack
                     strides = 2  # downsample
-                y = Resnet_layer(num_filters=num_filters, strides=strides)(inputs=x)
-                y = Resnet_layer(num_filters=num_filters, activation=None)(inputs=y)
+                y = resnet_layer(x, num_filters=num_filters, strides=strides)
+                y = resnet_layer(y, num_filters=num_filters, activation=None)
                 if stack > 0 and res_block == 0:  # first layer but not first stack
                     # linear projection residual shortcut connection to match
                     # changed dims
-                    x = Resnet_layer(
+                    x = resnet_layer(
+                        x,
                         num_filters=num_filters,
                         kernel_size=1,
                         strides=strides,
                         activation=None,
                         batch_normalization=False,
-                    )(inputs=x)
-                x = keras.layers.add([x, y])
+                    )
+                x = Add()([x, y])
                 x = Activation("relu")(x)
             num_filters *= 2
 
@@ -97,7 +106,9 @@ class Teacher_model(tf.keras.Model):
         y = Flatten()(x)
 
         # Attention! Here returns logits, no activation!
-        outputs = Dense(self.num_classes, activation=None, kernel_initializer="he_normal")(y)
+        outputs = Dense(
+            self.num_classes, activation=None, kernel_initializer="he_normal"
+        )(y)
 
         return outputs
 
@@ -140,3 +151,8 @@ class Student_model(tf.keras.Model):
 if __name__ == "__main__":
     teacher = Teacher_model(depth=50, num_classes=10)
     student = Student_model()
+    img = np.random.random(size=(1, 32, 32, 3))
+    print(teacher(img))
+    print(teacher.summary())
+    print(student(img))
+    print(student.summary())
