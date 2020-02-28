70c93040995ecf6a30ebfd5282049992beabfeae
zhuangzi926 <zhuangzi926@qq.com>
2020-02-28 21:44:30 +0800

    fix resnet's incompatibility with tf.keras

--- cifar10_model.py
+++ cifar10_model.py
@@ -8,15 +8,13 @@ from keras.models import Model, Sequential
 import numpy as np
 
 
-def resnet_layer(
-    inputs,
-    num_filters=16,
-    kernel_size=3,
-    strides=1,
-    activation="relu",
-    batch_normalization=True,
-    conv_first=True,
-):
+def resnet_layer(inputs,
+                 num_filters=16,
+                 kernel_size=3,
+                 strides=1,
+                 activation='relu',
+                 batch_normalization=True,
+                 conv_first=True):
     """2D Convolution-Batch Normalization-Activation stack builder
 
     # Arguments
@@ -32,14 +30,12 @@ def resnet_layer(
     # Returns
         x (tensor): tensor as input to the next layer
     """
-    conv = Conv2D(
-        num_filters,
-        kernel_size=kernel_size,
-        strides=strides,
-        padding="same",
-        kernel_initializer="he_normal",
-        kernel_regularizer=l2(1e-4),
-    )
+    conv = Conv2D(num_filters,
+                  kernel_size=kernel_size,
+                  strides=strides,
+                  padding='same',
+                  kernel_initializer='he_normal',
+                  kernel_regularizer=l2(1e-4))
 
     x = inputs
     if conv_first:
@@ -57,6 +53,80 @@ def resnet_layer(
     return x
 
 
+def resnet_v1(input_shape, depth, num_classes=10):
+    """ResNet Version 1 Model builder [a]
+
+    Stacks of 2 x (3 x 3) Conv2D-BN-ReLU
+    Last ReLU is after the shortcut connection.
+    At the beginning of each stage, the feature map size is halved (downsampled)
+    by a convolutional layer with strides=2, while the number of filters is
+    doubled. Within each stage, the layers have the same number filters and the
+    same number of filters.
+    Features maps sizes:
+    stage 0: 32x32, 16
+    stage 1: 16x16, 32
+    stage 2:  8x8,  64
+    The Number of parameters is approx the same as Table 6 of [a]:
+    ResNet20 0.27M
+    ResNet32 0.46M
+    ResNet44 0.66M
+    ResNet56 0.85M
+    ResNet110 1.7M
+
+    # Arguments
+        input_shape (tensor): shape of input image tensor
+        depth (int): number of core convolutional layers
+        num_classes (int): number of classes (CIFAR10 has 10)
+
+    # Returns
+        model (Model): Keras model instance
+    """
+    if (depth - 2) % 6 != 0:
+        raise ValueError('depth should be 6n+2 (eg 20, 32, 44 in [a])')
+    # Start model definition.
+    num_filters = 16
+    num_res_blocks = int((depth - 2) / 6)
+
+    inputs = Input(shape=input_shape)
+    x = resnet_layer(inputs=inputs)
+    # Instantiate the stack of residual units
+    for stack in range(3):
+        for res_block in range(num_res_blocks):
+            strides = 1
+            if stack > 0 and res_block == 0:  # first layer but not first stack
+                strides = 2  # downsample
+            y = resnet_layer(inputs=x,
+                             num_filters=num_filters,
+                             strides=strides)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters,
+                             activation=None)
+            if stack > 0 and res_block == 0:  # first layer but not first stack
+                # linear projection residual shortcut connection to match
+                # changed dims
+                x = resnet_layer(inputs=x,
+                                 num_filters=num_filters,
+                                 kernel_size=1,
+                                 strides=strides,
+                                 activation=None,
+                                 batch_normalization=False)
+            x = Add()([x, y])
+            x = Activation('relu')(x)
+        num_filters *= 2
+
+    # Add classifier on top.
+    # v1 does not use BN after last shortcut connection-ReLU
+    x = AveragePooling2D(pool_size=8)(x)
+    y = Flatten()(x)
+    outputs = Dense(num_classes,
+                    activation='softmax',
+                    kernel_initializer='he_normal')(y)
+
+    # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+
 class Teacher_model(tf.keras.Model):
     """teacher net
 
@@ -64,53 +134,15 @@ class Teacher_model(tf.keras.Model):
     Link: https://keras.io/examples/cifar10_resnet/
     """
 
-    def __init__(self, depth, num_classes=10):
+    def __init__(self):
         super(Teacher_model, self).__init__()
-        if (depth - 2) % 6 != 0:
-            raise ValueError("depth should be 6n+2 (eg 20, 32, 44 in [a])")
-        self.depth = depth
-        self.num_classes = num_classes
+        self.resnet = resnet_v1(
+            input_shape=(32, 32, 3),
+            depth=50, num_classes=10
+        )
 
     def call(self, inputs):
-        num_filters = 16
-        num_res_blocks = int((self.depth - 2) / 6)
-        inputs = tf.convert_to_tensor(inputs, dtype=tf.float32)
-        inputs = InputLayer(input_shape=(32, 32, 3))(inputs)
-        x = resnet_layer(inputs=inputs)
-        # Instantiate the stack of residual units
-        for stack in range(3):
-            for res_block in range(num_res_blocks):
-                strides = 1
-                if stack > 0 and res_block == 0:  # first layer but not first stack
-                    strides = 2  # downsample
-                y = resnet_layer(x, num_filters=num_filters, strides=strides)
-                y = resnet_layer(y, num_filters=num_filters, activation=None)
-                if stack > 0 and res_block == 0:  # first layer but not first stack
-                    # linear projection residual shortcut connection to match
-                    # changed dims
-                    x = resnet_layer(
-                        x,
-                        num_filters=num_filters,
-                        kernel_size=1,
-                        strides=strides,
-                        activation=None,
-                        batch_normalization=False,
-                    )
-                x = Add()([x, y])
-                x = Activation("relu")(x)
-            num_filters *= 2
-
-        # Add classifier on top.
-        # v1 does not use BN after last shortcut connection-ReLU
-        x = AveragePooling2D(pool_size=8)(x)
-        y = Flatten()(x)
-
-        # Attention! Here returns logits, no activation!
-        outputs = Dense(
-            self.num_classes, activation=None, kernel_initializer="he_normal"
-        )(y)
-
-        return outputs
+        return self.resnet(inputs)
 
 
 class Student_model(tf.keras.Model):
@@ -149,10 +181,5 @@ class Student_model(tf.keras.Model):
 
 
 if __name__ == "__main__":
-    teacher = Teacher_model(depth=50, num_classes=10)
+    teacher = Teacher_model()
     student = Student_model()
-    img = np.random.random(size=(1, 32, 32, 3))
-    print(teacher(img))
-    print(teacher.summary())
-    print(student(img))
-    print(student.summary())
